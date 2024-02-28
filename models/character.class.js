5f68c0c78ba6dab837ed273bc1ce93b0b39c0534
Roman Kinzele <romankinzel@outlook.dem>
2024-02-28 08:10:57 +0100

    jump

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,5 @@
 class Character extends MovableObject {
-  y = 180;
+  y = 80;
   height = 250;
   width = 130;
   speed = 10;
@@ -12,41 +12,63 @@ class Character extends MovableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
 
+  IMAGES_JUMP = [
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-33.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png",
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
+  ];
+
   world;
-  walking_sound = new Audio('audio/running.mp3');
+  walking_sound = new Audio("audio/running.mp3");
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMP);
+    this.applyGravity();
     this.animate();
   }
 
-  animate(){
+  animate() {
     setInterval(() => {
       this.walking_sound.pause();
-      if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+      if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
         this.x += this.speed;
         this.otherDirection = false;
         this.walking_sound.play();
       }
 
-      if(this.world.keyboard.LEFT && this.x > 0) {
+      if (this.world.keyboard.LEFT && this.x > 0) {
         this.x -= this.speed;
         this.otherDirection = true;
         this.walking_sound.play();
       }
 
-      this.world.camera_x = -this.x +100;
-    }, 1000 / 60 );
-    
+      // console.log('this.speedY', this.speedY)
+
+      if (this.world.keyboard.UP && this.y > 80) {
+        this.speedY = 20;
+      }
+
+      this.world.camera_x = -this.x + 100;
+    }, 1000 / 60);
+
     setInterval(() => {
-
-        if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMP);
+      } else {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
           // Walk animation
           this.playAnimation(this.IMAGES_WALKING);
         }
-    }, 50)
+      }
+    }, 50);
   }
 
   jump() {}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -9,12 +9,32 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
+
+    applyGravity() {
+        // Funktion, die die Schwerkraft auf das Objekt anwendet
+        // Diese Funktion wird in regelmäßigen Intervallen ausgeführt
+        setInterval(() => {
+            // Überprüfen, ob das Objekt noch über dem Boden ist (180 ist hier ein Platzhalterwert für die Bodenhöhe)
+            if (this.isAboveGround() || this.speedY > 0) {
+                // Reduziere die vertikale Position des Objekts basierend auf seiner vertikalen Geschwindigkeit
+                this.y -= this.speedY;
+                // Verringere die vertikale Geschwindigkeit des Objekts basierend auf der Beschleunigung
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25); // Intervall: Alle 25 Millisekunden
+    }
+
+    isAboveGround(){
+        return this.y < 180
+    }
+    
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
-
 
     /**
      * 
